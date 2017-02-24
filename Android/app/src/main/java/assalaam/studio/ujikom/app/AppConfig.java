fd854bd28bda1a359a89ccefbadc329018f67d12
smk-141510201 <141510201@smkassalaambandung.sch.id>
2017-02-24 20:45:49 +0700

    andoid login

---
+++ Android/app/src/main/java/assalaam/studio/ujikom/app/AppConfig.java
@@ -0,0 +1,9 @@
+package assalaam.studio.ujikom.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://192.168.2.101/ujikom-141510201/public/api/login";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://ujikom-2017.smkassalaambandung.sch.id/api/register";
+}
